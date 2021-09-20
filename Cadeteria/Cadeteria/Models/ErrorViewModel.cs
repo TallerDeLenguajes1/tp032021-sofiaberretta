c98038dd7171ba18193b17655d864f2d43d959b8
Ana Sofia Berretta Gali <anasofiaberrettagali@gmail.com>
2021-09-19 22:15:19 -0300

    Creacion de las entidades

---
+++ Cadeteria/Cadeteria/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cadeteria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
