8a67b1822e33daa1721f3466401ae03e61081664
Ana Sofia Berretta Gali <anasofiaberrettagali@gmail.com>
2021-10-11 15:21:10 -0300

    Modificado de cadetes en la vista

--- Cadeteria/Cadeteria/Controllers/CadeteController.cs
+++ Cadeteria/Cadeteria/Controllers/CadeteController.cs
@@ -10,7 +10,6 @@ namespace Cadeteria.Controllers
 {
     public class CadeteController : Controller
     {
-        static int id = 1;
         private readonly ILogger<CadeteController> _logger;
         private readonly DBTemporal _DB;
         public CadeteController(ILogger<CadeteController> logger, DBTemporal DB)
@@ -29,12 +28,81 @@ namespace Cadeteria.Controllers
         }
 
         public IActionResult crearCadete(string nombre, string direc, string tel)
-        { 
-            Cadete nuevoCadete = new Cadete(id, nombre, direc, tel);
-            id++;
+        {
+            int ultimoId = 0;
+            if (_DB.Cadeteria.ListaCadetes.Count() > 0)
+            {
+                ultimoId = _DB.Cadeteria.ListaCadetes.Max(x => x.Id);
+            }
+            ultimoId++;
+            Cadete nuevoCadete = new Cadete(ultimoId, nombre, direc, tel);
             _DB.Cadeteria.ListaCadetes.Add(nuevoCadete);
             _DB.GuardarCadete(_DB.Cadeteria.ListaCadetes);
             return View("MostrarCadetes", _DB.Cadeteria.ListaCadetes);
         }
+
+        public IActionResult eliminarCadete(int id) 
+        {
+            for(int i=0; i < _DB.Cadeteria.ListaCadetes.Count(); i++)
+            {
+                if(_DB.Cadeteria.ListaCadetes[i].Id == id)
+                {
+                    _DB.Cadeteria.ListaCadetes.Remove(_DB.Cadeteria.ListaCadetes[i]);
+                    break;
+                }
+            }
+
+            return View("MostrarCadetes", _DB.Cadeteria.ListaCadetes);
+        }
+
+        public IActionResult modificarCadete(int id)
+        {
+            Cadete cadeteAModificar = null;
+            for (int i = 0; i < _DB.Cadeteria.ListaCadetes.Count(); i++)
+            {
+                if (_DB.Cadeteria.ListaCadetes[i].Id == id)
+                {
+                    cadeteAModificar = _DB.Cadeteria.ListaCadetes[i];
+                    break;
+                }
+            }
+
+            if(cadeteAModificar != null)
+            {
+                return View("ModificarCadete", cadeteAModificar);
+            }
+            else
+            {
+                return Redirect("MostrarCadetes");
+            }
+        }
+
+        public IActionResult cambiarDatosCadete(int id, string nombre, string direc, string tel)
+        {
+            Cadete cadeteAModificar = null;
+            for (int i = 0; i < _DB.Cadeteria.ListaCadetes.Count(); i++)
+            {
+                if (_DB.Cadeteria.ListaCadetes[i].Id == id)
+                {
+                    cadeteAModificar = _DB.Cadeteria.ListaCadetes[i];
+                    break;
+                }
+            }
+
+            if (cadeteAModificar != null)
+            {
+                cadeteAModificar.Nombre = nombre;
+                cadeteAModificar.Direccion = direc;
+                cadeteAModificar.Telefono = tel;
+                return Redirect("MostrarCadetes");
+            }
+            else
+            {
+                return View("MostrarCadetes");
+            }
+        }
+
     }
+
+    
 }
